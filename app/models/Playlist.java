8bc042742504b657c5180a53053e866518caea8a
dermie83 <dermiemadsen@gmail.com>
2023-04-04 21:59:57 +0100

    Initial commit

---
+++ app/models/Playlist.java
@@ -0,0 +1,16 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Playlist
+{
+    public String title;
+    public List<Song> songs = new ArrayList<Song>();
+
+    public Playlist(String title)
+    {
+        this.title = title;
+    }
+}
+
